370550ee0c62e68e2a8f740dbeab674228f91b44
Gabriel Gramacho <108759445+Lowpexay@users.noreply.github.com>
2023-12-07 10:02:31 -0300

    Add files via upload

---
+++ ContadorJavascript-main/README.md
@@ -0,0 +1,14 @@
+# Contador 
+
+Um contador bem simples para testes :D
+
+| :computer: Vitrine.Dev |     |
+| -------------  | --- |
+| :sparkles: Nome        | **ContadorJS**
+| :label: Tecnologias | javascript, html, css (tecnologias utilizadas)
+| :rocket: URL         | https://lowpexay.github.io/ContadorJavascript/
+
+<!-- Inserir imagem com a #vitrinedev ao final do link -->
+![](https://faro.edu.br/wp-content/uploads/2018/01/163056-tarefa-para-2201-ate-18h-profissao-de-contador-conheca-os-principais-desafios-dessa-carreira.jpg#vitrinedev)
+
+
---
+++ ContadorJavascript-main/script.js
@@ -0,0 +1,34 @@
+// Contador Inicial
+var count = 0
+
+
+//Seleciona valor do botão
+
+const value = document.getElementById('value')
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function(e){
+        const styles =  e.currentTarget.classList
+        if(styles.contains('subtrai')){
+            count--
+        }
+        else if(styles.contains('soma')){
+            count++
+        } else {
+            count = 0
+        }
+
+        if(count > 0){
+            value.style.color = "#34A853"
+        }
+        else if(count < 0){
+            value.style.color = "#ea4335"
+        } else {
+            value.style.color = "#333"
+        }
+
+        
+        value.textContent = count
+    })
+})
